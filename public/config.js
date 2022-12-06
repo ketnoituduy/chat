b16dfe06b04f5163fd4629ea68b26b5535ad8e24
satthuhailua <satthuhailua@gmail.com>
2022-12-06 19:15:55 +0700

    update new 5

--- public/config.js
+++ public/config.js
@@ -1,5 +1,11 @@
 export const domain = 'https://chat-production-049e.up.railway.app/'
 export const domainRegister = 'https://chat-production-049e.up.railway.app/register'
 
-// export const domain = 'http://192.168.1.10:8000'
-// export const domainRegister = 'http://192.168.1.10:8000/register'
+// export const domain = 'http://192.168.1.10:3000'
+// export const domainRegister = 'http://192.168.1.10:3000/register'
+
+// export const DB_PORT = process.env.PORT || 3000
+// export const DB_HOST = process.env.DB_HOST || 'localhost'
+// export const DB_USER = process.env.DB_USER || 'root'
+// export const DB_PASSWORD = process.env.DB_PASSWORD || '123'
+// export const DB_NAME = process.env.DB_NAME || 'chatDatabase'
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+
+const DB_PORT = process.env.PORT || 3000
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER = process.env.DB_USER || 'root'
+const DB_PASSWORD = process.env.DB_PASSWORD || '123'
+const DB_NAME = process.env.DB_NAME || 'chatDatabase'
+
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 
-server.listen(process.env.PORT || 6348, ()=>{
-    console.log('listen port 8000')
+server.listen(DB_PORT, ()=>{
+    console.log('listen port 3000')
 })
 
 const router = express.Router()
@@ -19,11 +26,11 @@ app.use(bodyparser.json());
 //database
 const mysql = require('mysql')
 const dbChat = mysql.createConnection({
-    user:'root',
-    password:'TZzWwblGxwbo7o3fa9hE',
-    host:'containers-us-west-80.railway.app',
-    database:'railway',
-    port:6348
+    user:DB_USER,
+    password:DB_PASSWORD,
+    host:DB_HOST,
+    database:DB_NAME,
+    port:DB_PORT
 })
 
 // const dbChat = mysql.createConnection({
