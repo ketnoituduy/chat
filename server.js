b0318e2e5ce71d1012572baa9fe6216f22e26a7b
satthuhailua <satthuhailua@gmail.com>
2023-01-01 12:44:12 +0700

    update1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 
 
-const DB_PORT = process.env.PORT || 3000
+const DB_PORT = process.env.PORT || 6777
 // const DB_HOST = process.env.DB_HOST || 'localhost'
 // const DB_USER = process.env.DB_USER || 'root'
 // const DB_PASSWORD = process.env.DB_PASSWORD || '123'
@@ -12,7 +12,7 @@ const app = express()
 const server = require('http').Server(app)
 
 server.listen(DB_PORT, ()=>{
-    console.log('listen port 3000')
+    console.log('listen port 6777')
 })
 
 const router = express.Router()
